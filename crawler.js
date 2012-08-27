631671f2efc237172f6f80f10f693324e541b148
Patrick Weygand <patrick.weygand@gmail.com>
2012-08-27 03:05:51 +0000

    simple single request working

--- crawler.js
+++ crawler.js
@@ -2,17 +2,11 @@ var http = require('http'),
 	sys = require('sys');
 
 function Crawler () {
-	var server = http.createServer(function (request, response) {
-		request.on('end', function () {
-			var resp = sys.inspect(request);
-			response.writeHead(200, {
-				'content-type': 'text/plain',
-				'content-length': resp.length
-			});
-			response.write(resp);
-			response.end();
-		});
+	var client = http.get('http://google.com', function (res) {
+		console.log('got response' + res.statusCode);
 	});
-	server.listen(80);
-};
+	client.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+}
 module.exports = Crawler;
